73fb9f7b5772b6078f45b133defdf98d3a529d1f
Marc Fernandez <mfernandez225@gmail.com>
2020-04-08 23:42:17 -0700

    cleaning up to deploy

--- Develop/server.js
+++ Develop/server.js
@@ -8,23 +8,27 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
-app.use(require("./routes/html"));
+app.use(require("./routes/html.js"));
 app.use(require("./routes/api"));
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(`App listening on http://localhost:${PORT}`)
+  );
 });
